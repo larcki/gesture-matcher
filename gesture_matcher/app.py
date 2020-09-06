09a56b4fdfb6e0775ce6135a16f27a72cced3380
larcki <lassi.kapraali@gmail.com>
2020-09-06 12:00:13 +0100

    color for result and labels

--- gesture_matcher/app.py
+++ gesture_matcher/app.py
@@ -10,16 +10,27 @@ from gesture_matcher.recorder import Recorder
 kivy.require('1.11.0')
 
 
+def result_color(result):
+    if result > 0.89:
+        return [0, 1, 0, 1]
+    elif result > 0.69:
+        return [1, 1, 0, 1]
+    else:
+        return [1, 0, 0, 1]
+
+
 class MainGrid(Widget):
     record_text = ObjectProperty(None)
     match_text = ObjectProperty(None)
     recorder = Recorder()
 
     def on_record_button_click(self):
+        self.record_text.color = [1, 1, 1, 1]
         self.record_text.text = 'Get ready... 3'
         Clock.schedule_interval(self.get_ready_countdown, 1)
 
     def on_match_button_click(self):
+        self.match_text.color = [1, 1, 1, 1]
         self.match_text.text = 'Get ready... 3'
         Clock.schedule_interval(self.match_get_ready_countdown, 1)
 
@@ -28,6 +39,7 @@ class MainGrid(Widget):
             self.record_text,
             self.schedule_recording_countdown,
             lambda: 'Recording... 5',
+            lambda: [1, 0, 0, 1],
             'Get ready... '
         )
 
@@ -36,14 +48,17 @@ class MainGrid(Widget):
             self.match_text,
             self.schedule_matching_countdown,
             lambda: 'Recording... 5',
+            lambda: [1, 0, 0, 1],
             'Get ready... '
         )
 
     def schedule_recording_countdown(self):
+        self.record_text.color = [1, 1, 1, 1]
         self.recorder.start_recording()
         Clock.schedule_interval(self.recording_countdown, 1)
 
     def schedule_matching_countdown(self):
+        self.match_text.color = [1, 1, 1, 1]
         self.recorder.start_matching()
         Clock.schedule_interval(self.matching_countdown, 1)
 
@@ -52,6 +67,7 @@ class MainGrid(Widget):
             self.record_text,
             self.recorder.stop_recording,
             lambda: 'Saved',
+            lambda: [0, 1, 0, 1],
             'Recording... '
         )
 
@@ -60,6 +76,7 @@ class MainGrid(Widget):
             self.match_text,
             self.recorder.stop_matching,
             lambda: str(self.recorder.similarity),
+            lambda: result_color(self.recorder.similarity),
             'Recording... '
         )
 
--- gesture_matcher/countdown.py
+++ gesture_matcher/countdown.py
@@ -1,9 +1,10 @@
-def countdown(label_to_update, action, final_text, suffix):
+def countdown(label_to_update, action, final_text, final_text_color, suffix):
     current_value = label_to_update.text[-1]
     new_value = int(current_value) - 1
     if new_value <= 0:
         action()
         label_to_update.text = final_text()
+        label_to_update.color = final_text_color()
         return False
     label_to_update.text = suffix + str(new_value)
     return True
